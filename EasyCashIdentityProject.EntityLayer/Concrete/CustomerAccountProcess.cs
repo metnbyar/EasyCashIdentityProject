454cb92d088d19168f5127b3639b2e65676a0c2b
metnbyar <99257085+metnbyar@users.noreply.github.com>
2024-09-23 14:38:58 +0300

    cmt3

--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -12,6 +12,6 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
         public int CustomerAccountProcessID { get; set; }
         public string ProcessType { get; set; }
         public decimal Amount { get; set; }
-        public DateTime ProcessDate { get; set; }
+        public DateTime ProcessDate { get; set; } 
     }
 }
